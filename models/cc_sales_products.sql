0bbb79ff6cf809948235ae3073a9c58b361dcc0b
Victor Zaban <victorzaban28@gmail.com>
2023-08-08 18:00:06 +0000

    new commit with not_null and primary key tests

--- models/cc_sales_products.sql
+++ models/cc_sales_products.sql
@@ -1,11 +1,16 @@
-{{config(materialized='table')}}
+{{
+  config(
+    materialized='table'
+  )
+}}
 
-select
-    # ## Key ###  
-    product_id,
-    # ##########
-    sum(quantity) as qty_91,
-    round(sum(quantity) / 91, 2) as avg_daily_qty_91
-from `raw_data_circle.raw_cc_sales`
-where date_date >= date_sub('2021-10-01', interval 91 day)
-group by product_id
+SELECT
+  ### Key ###  
+  product_id
+  ###########
+  ,SUM(quantity) AS qty_91
+  ,ROUND(SUM(quantity)/91,2) AS avg_daily_qty_91
+FROM `raw_data_circle.raw_cc_sales` 
+WHERE 
+	date_date >= DATE_SUB('2021-10-01',INTERVAL 91 DAY)
+GROUP BY product_id
